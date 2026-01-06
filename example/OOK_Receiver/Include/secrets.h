89c71500899c73b83d82db565394a8106da447cb
nabrog87 <65133031+nabrog87@users.noreply.github.com>
2026-01-06 10:46:22 +0200

    Add secrets.h for WiFi and MQTT configuration

---
+++ example/OOK_Receiver/Include/secrets.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define WIFI_SSID     "YOUR_WIFI_SSID"
+#define WIFI_PASS     "YOUR_WIFI_PASS"
+
+#define MQTT_HOST     "192.168.1.10"
+#define MQTT_PORT     1883
+#define MQTT_USER     "mqtt_user"
+#define MQTT_PASS     "mqtt_password-"
+
+#define MQTT_TOPIC_OK  "rtl_433/vevor"
+#define MQTT_TOPIC_RAW "rtl_433/vevor/raw"
+#define MQTT_TOPIC_STAT "rtl_433/vevor/status"
+
